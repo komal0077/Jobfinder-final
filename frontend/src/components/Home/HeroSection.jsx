8beb73ea12a9943e03cdf73d037f8c6486d80d93
komal <komalme7@gmail.com>
2024-09-25 23:05:36 +0530

    final

--- frontend/src/components/Home/HeroSection.jsx
+++ frontend/src/components/Home/HeroSection.jsx
@@ -36,9 +36,8 @@ const HeroSection = () => {
             <h1>Find a job that suits</h1>
             <h1>your interests and skills</h1>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-              voluptate repellat modi quidem aliquid eaque ducimus ipsa et,
-              facere mollitia!
+            A job finder app typically helps users search for employment 
+            opportunities based on various criteria like location, job type, and industry
             </p>
           </div>
           <div className="image">
